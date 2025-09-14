5f6ae6c6b2f898a50c62374d77507bc53e6d9a8d
ChannabasavVectorIQ <ckoti@vectoriq.ai>
2025-09-14 17:17:25 +0530

    Update get_settings.py

--- get_settings.py
+++ get_settings.py
@@ -30,8 +30,8 @@ async def get_settings():
     try:
         # Query to get the row with maximum update_id (latest entry)
         query = """
-            SELECT * FROM azaisearch_ocm_settings2
-            WHERE update_id = (SELECT MAX(update_id) FROM azaisearch_ocm_settings2)
+            SELECT * FROM azaisearch_emo_settings
+            WHERE update_id = (SELECT MAX(update_id) FROM azaisearch_emo_settings)
         """
         row = await conn.fetchrow(query)
 
